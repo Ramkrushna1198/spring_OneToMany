a76723108e3eeb6dc46b7d1ddb39e25f9ad2d48f
Ramkrushna1198 <kushareramkrushna1198@gmail.com>
2022-06-29 12:25:43 +0530

    first commit

---
+++ src/main/java/com/springonetomany/controller/categoryRepo.java
@@ -0,0 +1,18 @@
+package com.springonetomany.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import java.io.Serializable;
+//import java.util.Optional;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//import com.springonetomany.entities.product;
+@Repository
+public interface categoryRepo extends JpaRepository<category, Integer>{
+
+//some extra methods related to JPA such as delete records
+
+}
---
+++ src/main/java/com/springonetomany/controller/productService.java
@@ -0,0 +1,56 @@
+package com.springonetomany.controller;
+
+	
+
+	import java.util.List;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.data.domain.Page;
+	import org.springframework.data.domain.PageRequest;
+	import org.springframework.data.domain.Pageable;
+	import org.springframework.data.repository.CrudRepository;
+	import org.springframework.stereotype.Service;
+
+
+	@Service
+	public class productService {
+
+		@Autowired
+		private productRepo pr;
+		
+		
+		
+		//department-@onetomany//cat
+		//employee             //pro
+		public product addorupdate(product p)
+		{
+			
+			return pr.save(p);
+		}
+		
+		public boolean delete(int id)
+		{
+			pr.deleteById(id);
+			return true;
+		}
+		
+		
+		public Iterable<product> getAll(Integer pageNumber,Integer pageSize)
+		{
+			Pageable p=PageRequest.of(pageNumber, pageSize);
+			
+			Page<product> p1=pr.findAll(p);
+			Iterable<product> c2=p1.getContent();
+			return c2;
+	//return pr.findAll();
+		}
+		
+		public product getById(int id) {
+			return pr.findById(id).get();//return cr.findById(id).get();
+		}
+		
+		
+	}
+
+
+
